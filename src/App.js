cbeb066428c52127c041bf98fa10d4367596030a
muskanbardia1 <muskanbardia98@gmail.com>
2021-02-03 17:18:55 +0530

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div >
-      hi
-    </div>
+    <Switch>
+
+      <Route exact path="/">
+        This is home page
+      </Route>
+
+      <Route exact path="/starred">
+        This is Starred
+      </Route>
+
+      <Route>
+        This is Error 404!!!!
+      </Route>
+
+    </Switch>
   );
 }
 
