ac0c36c263bf5b84064e65f5ed9d33532e1bff52
muskanbardia1 <muskanbardia98@gmail.com>
2021-02-05 16:19:38 +0530

    deployed the app and start using hash-router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-    ,document.getElementById('root'));
+ReactDOM.render(<HashRouter><App /></HashRouter>,document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
